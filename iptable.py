8faa7dba0268294a7ae37e1cc55aebf5962faa74
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-18 19:39:37 +0000

    FIRST

--- iptable.py
+++ iptable.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,17 +9,17 @@ def hello():
     return render_template("index.html")
 
 @app.route("/start")
-def index():
-    return render_template("connexion.html")
+def start():
+    return render_template("index.html")
 
-@app_route("/alias")
-def alias():
-    return render_template("alias.html")
-
-@app.route("/rules_nat_add")
-def rules_add():
+@app.route("/rules_nat")
+def rules_nat():
     return render_template("nat.html")
 
-@app.rotue("/rules_filters")
-def rules_filetrs():
-    return render_template("rules_add.html")
+@app.route("/rules_nat_add")
+def rules_nat_add():
+    return render_template("regle.html")
+
+@app.route("/alias")
+def alias():
+    return render_template("alias.html")
