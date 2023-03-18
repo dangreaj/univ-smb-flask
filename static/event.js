ac700b16f3e0ccf42613bf42489df00325974d96
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-18 19:07:27 +0000

    first

---
+++ static/event.js
@@ -0,0 +1,14 @@
+function info()
+{
+    alert("Voici les informations du systèmes");
+}
+
+function nat()
+{
+    alert("Table de translation des adresses");
+}
+
+function alias()
+{
+    alert("Alias des information sur les serveur et port utilisé");
+}
